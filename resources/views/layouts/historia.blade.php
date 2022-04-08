7fbb8bbf3740bc9e43c71fb27191f736ebce8c11
LuisGerardo17 <you@example.com>
2022-04-07 21:40:58 -0500

    history

--- resources/views/layouts/historia.blade.php
+++ resources/views/layouts/historia.blade.php
@@ -108,7 +108,7 @@
 	<main>
 		<nav aria-label="breadcrumb">
 			<ol class="breadcrumb ">
-				<li class="breadcrumb-item"><a href='homepage'>Principal</a></li>
+				<li class="breadcrumb-item"><a href='homepage.html'>Principal</a></li>
 				<li class="breadcrumb-item active" aria-current="page">Historia</li>
 			</ol>
 		</nav>
