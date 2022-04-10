9e1a37ff33f6c9feea20d81dcc0f6c1b540e3601
LuisGerardo17 <you@example.com>
2022-04-10 13:44:10 -0500

    controladores

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
-use App\Http\Controllers\historia;
+
 use Illuminate\Support\Facades\Route;
+//agragamos los controladores
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\CursosController;
+use App\Http\Controllers\ActividadesController;
+use App\Http\Controllers\RecursosController;
+use App\Http\Controllers\RolesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,15 +25,18 @@ Route::get('/', function () {
     return view('homepage');
 });
 
-
-Route::get('/', function () {
-    return view('historia');
-});
-
-
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware'=>['auth']],function(){
+Route::resource('roles',RolesController::class);
+Route::resource('usuarios',UsuariosController::class);
+Route::resource('cursos', CursosController::class);
+Route::resource('actividades', ActividadesController::class);
+Route::resource('recursos', RecursosController::class);
+
+
+});
