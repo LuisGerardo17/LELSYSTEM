807940eaa5473ec81e0dc2544f8918ed27366035
LuisGerardo17 <you@example.com>
2022-04-12 16:14:39 -0500

    menu

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -3,15 +3,15 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h3 class="page__heading">Usuarios</h3>
+            <h3 class="page__heading">ADMINISTRADROR</h3></h3>
         </div>
         <div class="section-body">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
-                            <h3 class="text-center">ADMINISTRADROR</h3>
-                            <a class="btn-btn warning" href="{{route('usuarios.create')}}">Nuevo</a>
+                            <h3 class="text-center">Usuarios</h3>
+                            <a class="btn btn-warning" href="{{route('usuarios.create')}}">Nuevo</a>
                         </div>
                     </div>
                 </div>
