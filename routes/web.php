b66e5c54f270ea033bb49ba7e3fa1ef33f2262c2
LuisGerardo17 <you@example.com>
2022-04-14 16:36:27 -0500

    auth

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/', function () {
 });
 
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 
