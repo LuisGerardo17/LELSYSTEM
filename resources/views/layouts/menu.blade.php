225e67743d90e674df47c47f368e25f7f0073af0
LuisGerardo17 <you@example.com>
2022-04-20 17:24:05 -0500

    cambios

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,5 +1,24 @@
 <li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
-    <a class="nav-link" href="/">
-        <i class=" fas fa-building"></i><span>Dashboard</span>
+    <a class="nav-link" href="/login">
+        <i class=" fas fa-login"></i><span>Salir</span>
+    </a>
+
+     <a class="nav-link" href="/usuarios">
+        <i class=" fas fa-user"></i><span>Usuarios</span>
+    </a>
+
+     <a class="nav-link" href="/roles">
+        <i class=" fas fa-roles"></i><span>Roles</span>
+    </a>
+
+     <a class="nav-link" href="/cursos">
+        <i class=" fas fa-book"></i><span>Cursos</span>
+    </a>
+
+     <a class="nav-link" href="/actividades">
+        <i class=" fas fa-blog"></i><span>Actividades</span>
+    </a>
+     <a class="nav-link" href="/recursos">
+        <i class=" fas fa-building"></i><span>Recursos</span>
     </a>
 </li>
