0167554d6ea55c085a03eeda95b140b16f8c62b1
LuisGerardo17 <you@example.com>
2022-04-21 12:06:55 -0500

    cambi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,15 +11,15 @@
                     <div class="card">
                         <div class="card-body">
                           <div class="row">
-                              <div class="col-md-4 col-xl-4">
+                               <div class="col-md-4 col-xl-4">
 
-                                <div class="card-bg-c-blue order-card">
+                                  <div class="card-bg-c-blue order-card">
 
-                                   <div class = "card-block">
-                                      <h5>Usuarios</h5>
-                                      <h2 class="text-right"><i class="fas fa-users f-left"></i><spn></spn></h2>
-                                      <p class="m-b-0 text-right"><a href="/usuarios" class="text-white">Ver más </a></p>
-                                   </div>
+                                       <div class = "card-block">
+                                          <h5>Usuarios</h5>
+                                          <h2 class="text-right"><i class="fas fa-users f-left"></i><spn></spn></h2>
+                                        <p class="m-b-0 text-right"><a href="/usuarios" class="text-white">Ver más </a></p>
+                                       </div>
                                 </div>
                               </div>
                             </div>
